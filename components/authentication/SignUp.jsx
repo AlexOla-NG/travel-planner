379bc14ad8b1c07e2ab8371fa9cca0a9dc790b2c
Alx Oweka <alexanderolalekanoweka@gmail.com>
2023-12-09 14:02:51 +0100

    🔥feat: setup middleware using NextAuth.js

--- components/authentication/SignUp.jsx
+++ components/authentication/SignUp.jsx
@@ -2,10 +2,6 @@ import Link from 'next/link';
 import React from 'react'
 import { signIn } from 'next-auth/react';
 
-// TODO: stopped here
-// create models, middleware for api
-// api middleware should include check for authentication to prevent unauthorized api access
-
 const SignUp = () => {
 
   const handleClick = (e) => {
