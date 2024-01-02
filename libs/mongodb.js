4f00d63032d85738ab9122750be169952c6d26ee
Alx Oweka <alexanderolalekanoweka@gmail.com>
2024-01-02 06:27:28 +0100

    🔥feat: added mongodb caching

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,35 +2,53 @@ import mongoose from "mongoose";
 
 const MONGO_DB_URI = process.env.MONGO_DB_URI;
 
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  bufferCommands: false,
+  maxIdleTimeMS: 10000,
+  serverSelectionTimeoutMS: 10000,
+  socketTimeoutMS: 20000,
+};
+
+// TODO: stopped here
+// server returns 500 on first connection, please fix
+
+// TODO: implement recycled promises
+// see: https://github.com/vercel/next.js/discussions/12229#discussioncomment-45826
+const readyStates = {
+  disconnected: 0,
+  connected: 1,
+  connecting: 2,
+  disconnecting: 3,
+};
+let pendingPromise = null
+
+
+if (!MONGO_DB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGO_DB_URI"');
+}
+
 export const connectMongoDB = async () => {
-  if (!MONGO_DB_URI) {
-    throw new Error('Invalid/Missing environment variable: "MONGO_DB_URI"');
-  }
 
-  // try {
-  //   if (process.env.NODE_ENV === 'development') {
-  //     let globalWithMongo = global;
-  //     // console.log('inside connectMongoDB globalWithMongo here:', globalWithMongo._mongoClientPromise.Promise.s);
-  //     // console.log('inside connectMongoDB globalWithMongo here:', globalWithMongo._mongoClientPromise);
-
-  //     if (!globalWithMongo._mongoClientPromise) {
-  //       globalWithMongo._mongoClientPromise = await mongoose.connect(MONGO_DB_URI);
-  //       console.log('Connected to MongoDB in development mode');
-  //     }
-  //   } else {
-  //     await mongoose.connect(MONGO_DB_URI);
-  //     console.log('Connected to MongoDB in production mode');
-  //   }
-  // } catch (error) {
-  //   console.error('Error connecting to MongoDB:', error);
-  //   throw error; // Re-throw the error for further handling
-  // }
-  
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI)
-    console.log('connected to mongoDB');
+    // fix for mongodb cache issue
+    // https://github.com/vercel/next.js/discussions/12229#discussioncomment-91070
+    if (global.connection) return global.connection
+  
+    if (!global.connectionPromise) {
+      global.connectionPromise = mongoose.connect(MONGO_DB_URI, options)
+      console.log('connected to mongoDB');
+    }
+  
+    const client = await global.connectionPromise
+    global.connection = client
+  
+    return global.connection
+    
   } catch (error) {
     console.log('error connecting to mongoDB:', error);
     throw error; // Re-throw the error for further handling
   }
+
 };
