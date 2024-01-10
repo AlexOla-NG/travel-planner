2d6306577f25194e63b6eac4652856d20e919c93
Alx Oweka <alexanderolalekanoweka@gmail.com>
2024-01-10 06:08:16 +0100

    🔥feat: refactored api routes for getting all users

--- controllers/user/getUsers.js
+++ controllers/user/getUsers.js
@@ -1,5 +1,10 @@
 import User from "@/models/User"
 
-export default async function getUsers() {
-  return await User.find()
+export default async function getUsers(res) {
+  const users = await User.find()
+
+  if (!users)
+    return res.status(404).json({ message: 'No users in db' });
+
+  return res.status(200).json({ message: 'success', data: users })
 }
--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,28 +1,25 @@
 import getUsers from "@/controllers/user/getUsers";
 import { connectMongoDB } from "@/libs/mongodb";
-import User from "@/models/User";
 
-// connect to db
-connectMongoDB()
 
 // TODO: implement advanced results function to handle sorting, filtering, pagination, etc
 
 // get all users
 export default async function handler(req, res) {
+  // connect to db
+  await connectMongoDB()
+  
   const { method } = req
   try {
-
     if (method !== 'GET') {
       return res
-        .status(400)
+        .status(405)
         .json({ success: false, message: 'Only GET requests are allowed.' });
     }
 
-    const data = await User.find()
-    // const data = await getUsers()
-
-    return res.status(200).json({ message: 'success', data })
+    await getUsers(res)
   } catch (error) {
+    console.error(error)
     return res.status(500).json({ message: 'An error occured while fetching users.' })
   }
 }
