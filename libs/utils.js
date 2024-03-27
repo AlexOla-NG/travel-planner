c26124cd733eeef03180e08bcaaee08952f3d578
Alx Oweka <alexanderolalekanoweka@gmail.com>
2024-03-27 22:33:24 +0100

    🔥feat: setup react toastify

--- libs/utils.js
+++ libs/utils.js
@@ -37,7 +37,6 @@ export const NotificationTypes = {
   ERROR: "error",
   INFO: "info",
   WARN: "warn",
-  UPLOAD: "upload",
 };
 
 /**
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,31 @@
 /* eslint-disable react/prop-types */
-import { SessionProvider } from "next-auth/react";
 
 import "@styles/global.scss";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "libs/react-query";
+import { SessionProvider } from "next-auth/react";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+        <ToastContainer
+          autoClose={2000}
+          closeOnClick
+          draggable
+          hideProgressBar={false}
+          newestOnTop={false}
+          pauseOnFocusLoss
+          pauseOnHover
+          position="top-right"
+          rtl={false}
+          theme="light"
+        />
+      </SessionProvider>
+    </QueryClientProvider>
   );
 }
