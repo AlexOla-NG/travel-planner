b221d83e7e920a531268f6ada580ed6cbb6bdd48
Alx Oweka <alexanderolalekanoweka@gmail.com>
2024-04-18 22:06:00 +0100

    🔥feat: setup nodemailer

---
+++ libs/nodemailer.js
@@ -0,0 +1,10 @@
+import nodemailer from "nodemailer";
+
+export const nodemailerTransporter = nodemailer.createTransport({
+  host: process.env.EMAIL_SERVER_HOST,
+  port: process.env.EMAIL_SERVER_PORT,
+  auth: {
+    user: process.env.EMAIL_SERVER_USER,
+    pass: process.env.EMAIL_SERVER_PASSWORD,
+  },
+});
