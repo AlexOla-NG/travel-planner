ed876579ecf01601c550ca46879aaf258849cd89
Alx Oweka <alexanderolalekanoweka@gmail.com>
2024-04-25 22:09:45 +0100

    🔥feat: added custom errors for APIs

---
+++ api/constants.js
@@ -0,0 +1,22 @@
+/**
+ * The different constants/types used for managing component/app related properties
+ */
+export default {
+  errors: {
+    alreadyExists: {
+      message: "already exists",
+      code: "already_exist",
+      description: "the resource is already in the database",
+    },
+    doesNotExist: {
+      message: "does not exist",
+      code: "does_not_exist",
+      description: "the resource does not exist",
+    },
+    invalidDetails: {
+      message: "invalid or incorrect",
+      code: "invalid_details",
+      description: "the resource is invalid or incorrect",
+    },
+  },
+};
